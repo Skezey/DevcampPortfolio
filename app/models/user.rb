777584cfcccfaa6925173c68c4249f39721ff0ca
Scott Young <youngscottj@gmail.com>
2019-04-16 12:04:58 -0600

    Add virtual attributes for user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  #Ensures that a name is entered during registration
+  validates_presence_of :name
+
+  #Splits entered name into first and last name
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
+
 end
