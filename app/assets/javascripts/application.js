f5d0cc96749f76a84e9034977959a45eacea7b5d
Scott Young <youngscottj@gmail.com>
2019-05-08 02:42:20 -0600

    Work on persistant drag and drop interface

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require turbolinks
 //= require_tree .
 //= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 //= require jquery-ui
 //= require html.sortable
+//= require popper
+//= require bootstrap-sprockets
