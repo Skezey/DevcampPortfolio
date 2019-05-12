698c59984e639534a3ac3a8073cd9cd8a2f3c2a9
Scott Young <youngscottj@gmail.com>
2019-05-12 16:21:40 -0600

    Update file upload forms and refractor place holder functionality into view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery3
+//= require jquery-ui
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery3
-//= require jquery-ui
 //= require html.sortable
 //= require popper
 //= require bootstrap-sprockets
