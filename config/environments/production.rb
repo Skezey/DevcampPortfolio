cb5b2d0ac07d7fe1186bef298001582c1acf1a52
Scott Young <youngscottj@gmail.com>
2019-05-23 02:52:20 -0600

    Integrate production calls for ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://scottyoungportfolio.herokuapp.com', 'http://scottyoungportfolio.herokuapp.com']
+  config.action_cable.url = "wss://scottyoungportfolio.herokuapp.com/cable"
 end
